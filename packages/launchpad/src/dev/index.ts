4d1a2f27b62bc3aa2ed7b6acc38c627a95005623
Chris <chrisbreuer93@gmail.com>
2025-05-30 12:34:43 -0700

    chore: wip

--- packages/launchpad/src/dev/index.ts
+++ packages/launchpad/src/dev/index.ts
@@ -1,6 +1,6 @@
 // Export all dev functionality - now libpkgx-free
-export { default as dump } from './dump.ts'
-export { default as integrate } from './integrate.ts'
-export { default as shell_escape } from './shell-escape.ts'
-export { datadir, default as shellcode } from './shellcode().ts'
-export { default as sniff } from './sniff.ts'
+export { default as dump } from './dump'
+export { default as integrate } from './integrate'
+export { default as shell_escape } from './shell-escape'
+export { datadir, default as shellcode } from './shellcode'
+export { default as sniff } from './sniff'
