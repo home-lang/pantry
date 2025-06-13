f2881021696bfc18c630f3ef85c5772f281c400e
Chris <chrisbreuer93@gmail.com>
2025-06-13 11:22:17 -0700

    chore: wip

--- packages/launchpad/src/dev/shellcode.ts
+++ packages/launchpad/src/dev/shellcode.ts
@@ -1,4 +1,4 @@
-import { existsSync, readdirSync } from 'node:fs'
+import { existsSync } from 'node:fs'
 import path, { join } from 'node:path'
 import process from 'node:process'
 import { config } from '../config'
