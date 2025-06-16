22059126661ffe892362a64e58542fabd460d836
Chris <chrisbreuer93@gmail.com>
2025-06-16 13:50:36 -0700

    chore: lint

--- packages/launchpad/test/clean-fix.test.ts
+++ packages/launchpad/test/clean-fix.test.ts
@@ -1,7 +1,6 @@
 import { afterEach, beforeEach, describe, expect, test } from 'bun:test'
 import fs from 'node:fs'
 import path from 'node:path'
-import { install } from '../src/install'
 
 describe('Clean Command Fix - Package and Binary Removal', () => {
   let tempDir: string
