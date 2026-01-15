ebe9e439493e3e893f28a30c463092ccbe47fc34
glennmichael123 <gtorregosa@gmail.com>
2026-01-15 17:31:35 +0800

    chore: wip

---
+++ pickier.config.ts
@@ -0,0 +1,51 @@
+import type { PickierConfig } from 'pickier'
+
+const config: PickierConfig = {
+  verbose: false,
+  ignores: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/.zig-cache/**',
+    '**/zig-out/**',
+    '**/pantry/**',
+    '**/*.generated.zig',
+    '**/no/**',
+  ],
+
+  lint: {
+    extensions: ['ts', 'js', 'mts', 'cts'],
+    reporter: 'stylish',
+    cache: false,
+    maxWarnings: -1,
+  },
+
+  format: {
+    extensions: ['ts', 'js', 'mts', 'cts', 'json', 'md', 'yaml', 'yml'],
+    trimTrailingWhitespace: true,
+    maxConsecutiveBlankLines: 1,
+    finalNewline: 'one',
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+  },
+
+  rules: {
+    noDebugger: 'error',
+    noConsole: 'off',
+  },
+
+  pluginRules: {
+    // Markdown rules
+    'markdown/heading-increment': 'warn',
+    'markdown/no-trailing-spaces': 'error',
+    'markdown/fenced-code-language': 'warn',
+
+    // Disable overly strict rules
+    'regexp/no-unused-capturing-group': 'off',
+    'ts/no-top-level-await': 'off',
+    'style/brace-style': 'off',
+  },
+}
+
+export default config
