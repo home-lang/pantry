b5050c329e943f4997de7f23475fcd5699b5e0c4
Chris <chrisbreuer93@gmail.com>
2026-06-05 09:16:58 -0700

    feat(recipes): curlie.io prebuilt download

--- packages/ts-pantry/src/recipes/curlie.io.ts
+++ packages/ts-pantry/src/recipes/curlie.io.ts
@@ -11,18 +11,23 @@ export const recipe: Recipe = {
     type: 'github-releases',
     repo: 'rs/curlie',
   },
-  distributable: {
-    url: 'git+https://github.com/rs/curlie',
-  },
-  buildDependencies: {
-    'go.dev': '~1.24',
-    'git-scm.org': '*',
-  },
+  // Prebuilt download: curlie ships official per-platform release tarballs
+  // (bare `curlie` binary at the archive root).
+  distributable: null,
 
   build: {
     script: [
-      'mkdir -p {{prefix}}/bin',
-      'go build -ldflags="-s -w" -o {{prefix}}/bin/curlie .',
+      'VERSION={{version}}',
+      'case {{hw.platform}}+{{hw.arch}} in',
+      '  darwin+aarch64) ASSET="curlie_${VERSION}_darwin_arm64" ;;',
+      '  darwin+x86-64)  ASSET="curlie_${VERSION}_darwin_amd64" ;;',
+      '  linux+aarch64)  ASSET="curlie_${VERSION}_linux_arm64"  ;;',
+      '  linux+x86-64)   ASSET="curlie_${VERSION}_linux_amd64"  ;;',
+      'esac',
+      '',
+      'curl -Lfo curlie.tar.gz "https://github.com/rs/curlie/releases/download/v${VERSION}/${ASSET}.tar.gz"',
+      'tar xf curlie.tar.gz',
+      'install -Dm755 curlie {{prefix}}/bin/curlie',
     ],
   },
 }
