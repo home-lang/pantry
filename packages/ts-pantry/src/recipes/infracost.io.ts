b7375fda148eba1b3f43f37cea625e515978d6eb
Chris <chrisbreuer93@gmail.com>
2026-06-05 09:16:59 -0700

    feat(recipes): infracost.io prebuilt download

--- packages/ts-pantry/src/recipes/infracost.io.ts
+++ packages/ts-pantry/src/recipes/infracost.io.ts
@@ -11,28 +11,23 @@ export const recipe: Recipe = {
     type: 'github-releases',
     repo: 'infracost/infracost',
   },
-  distributable: {
-    url: 'https://github.com/infracost/infracost/archive/refs/tags/v{{version}}.tar.gz',
-    stripComponents: 1,
-  },
-  buildDependencies: {
-    'go.dev': '^1.19',
-  },
+  // Prebuilt download: infracost ships official per-platform release tarballs
+  // (binary named infracost-<os>-<arch> inside the archive).
+  distributable: null,
 
   build: {
     script: [
-      'go mod download',
-      'mkdir -p {{prefix}}/bin',
-      'go build -v -trimpath -ldflags="$LDFLAGS" -o $BUILDLOC ./cmd/infracost',
+      'VERSION={{version}}',
+      'case {{hw.platform}}+{{hw.arch}} in',
+      '  darwin+aarch64) ASSET="infracost-darwin-arm64" ;;',
+      '  darwin+x86-64)  ASSET="infracost-darwin-amd64" ;;',
+      '  linux+aarch64)  ASSET="infracost-linux-arm64"  ;;',
+      '  linux+x86-64)   ASSET="infracost-linux-amd64"  ;;',
+      'esac',
       '',
+      'curl -Lfo infracost.tar.gz "https://github.com/infracost/infracost/releases/download/v${VERSION}/${ASSET}.tar.gz"',
+      'tar xf infracost.tar.gz',
+      'install -Dm755 "${ASSET}" {{prefix}}/bin/infracost',
     ],
-    env: {
-      'GOPROXY': 'https://proxy.golang.org,direct',
-      'GOSUMDB': 'sum.golang.org',
-      'GO111MODULE': 'on',
-      'CGO_ENABLED': '0',
-      'BUILDLOC': '{{prefix}}/bin/infracost',
-      'LDFLAGS': ['-s', '-w', '-X github.com/infracost/infracost/internal/version.Version={{version}}'],
-    },
   },
 }
