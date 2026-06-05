3943bd01048444f1802d612720e379413e50cbe4
Chris <chrisbreuer93@gmail.com>
2026-06-05 11:41:44 -0700

    feat(recipes): upx prebuilt download on linux

--- packages/ts-pantry/src/recipes/upx.github.io.ts
+++ packages/ts-pantry/src/recipes/upx.github.io.ts
@@ -11,6 +11,9 @@ export const recipe: Recipe = {
     type: 'github-releases',
     repo: 'upx/upx',
   },
+  // Upstream ships official prebuilt Linux binaries (amd64/arm64) for every
+  // release. macOS has no official prebuilt, so darwin still compiles from
+  // the source tarball.
   distributable: {
     url: 'https://github.com/upx/upx/releases/download/{{version.tag}}/upx-{{version}}-src.tar.xz',
     stripComponents: 1,
@@ -19,14 +22,27 @@ export const recipe: Recipe = {
     'zlib.net': '*',
   },
   buildDependencies: {
-    'cmake.org': '*',
+    darwin: {
+      'cmake.org': '*',
+    },
   },
 
   build: {
     script: [
-      'cmake -S . -B build $CMAKE_ARGS',
-      'cmake --build build',
-      'cmake --install build',
+      'if [ "{{hw.platform}}" = "linux" ]; then',
+      '  case {{hw.arch}} in',
+      '    aarch64) ARCH="arm64" ;;',
+      '    x86-64)  ARCH="amd64" ;;',
+      '  esac',
+      '  URL="https://github.com/upx/upx/releases/download/{{version.tag}}/upx-{{version}}-${ARCH}_linux.tar.xz"',
+      '  curl -Lfo upx.tar.xz "$URL"',
+      '  tar Jxf upx.tar.xz',
+      '  install -Dm755 "upx-{{version}}-${ARCH}_linux/upx" {{prefix}}/bin/upx',
+      'else',
+      '  cmake -S . -B build $CMAKE_ARGS',
+      '  cmake --build build',
+      '  cmake --install build',
+      'fi',
     ],
     env: {
       'CMAKE_ARGS': ['-DCMAKE_INSTALL_PREFIX={{prefix}}', '-DCMAKE_BUILD_TYPE=Release', '-Wno-dev'],
