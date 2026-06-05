f0e826b455b0fa69d1d044de9b0a3c7d3f1af5ff
Chris <chrisbreuer93@gmail.com>
2026-06-05 09:16:59 -0700

    feat(recipes): kubectx.dev prebuilt download

--- packages/ts-pantry/src/recipes/kubectx.dev.ts
+++ packages/ts-pantry/src/recipes/kubectx.dev.ts
@@ -11,23 +11,29 @@ export const recipe: Recipe = {
     type: 'github-releases',
     repo: 'ahmetb/kubectx',
   },
-  distributable: {
-    url: 'git+https://github.com/ahmetb/kubectx.git',
-  },
   dependencies: {
     'github.com/junegunn/fzf': '*',
   },
-  buildDependencies: {
-    'go.dev': '^1.20',
-  },
+  // Prebuilt download: kubectx ships official per-platform release tarballs for
+  // each binary (kubectx_v<ver>_<os>_<arch>.tar.gz, kubens_v<ver>_...).
+  distributable: null,
 
   build: {
     script: [
-      'go build -o \'{{prefix}}/bin/kubectx\' ./cmd/kubectx',
-      'go build -o \'{{prefix}}/bin/kubens\' ./cmd/kubens',
+      'VERSION={{version}}',
+      'case {{hw.platform}}+{{hw.arch}} in',
+      '  darwin+aarch64) SUFFIX="darwin_arm64"  ;;',
+      '  darwin+x86-64)  SUFFIX="darwin_x86_64" ;;',
+      '  linux+aarch64)  SUFFIX="linux_arm64"   ;;',
+      '  linux+x86-64)   SUFFIX="linux_x86_64"  ;;',
+      'esac',
+      '',
+      'curl -Lfo kubectx.tar.gz "https://github.com/ahmetb/kubectx/releases/download/v${VERSION}/kubectx_v${VERSION}_${SUFFIX}.tar.gz"',
+      'curl -Lfo kubens.tar.gz "https://github.com/ahmetb/kubectx/releases/download/v${VERSION}/kubens_v${VERSION}_${SUFFIX}.tar.gz"',
+      'tar xf kubectx.tar.gz',
+      'tar xf kubens.tar.gz',
+      'install -Dm755 kubectx {{prefix}}/bin/kubectx',
+      'install -Dm755 kubens {{prefix}}/bin/kubens',
     ],
-    env: {
-      'CGO_ENABLED': '0',
-    },
   },
 }
