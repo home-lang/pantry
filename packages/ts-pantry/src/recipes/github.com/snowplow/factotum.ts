1b8d7db9b178d5ccfab9e898f3f6170fef7c4d35
Chris <chrisbreuer93@gmail.com>
2026-06-06 16:23:55 -0700

    fix: skip mislabeled factotum binaries

--- packages/ts-pantry/src/recipes/github.com/snowplow/factotum.ts
+++ packages/ts-pantry/src/recipes/github.com/snowplow/factotum.ts
@@ -9,7 +9,7 @@ export const recipe: Recipe = {
   dependencies: {
     'openssl.org': '^3',
   },
-  platforms: ['darwin/aarch64', 'darwin/x86-64', 'linux/x86-64', 'linux/aarch64'],
+  platforms: ['darwin/aarch64', 'linux/x86-64'],
   versionSource: {
     type: 'github-releases',
     repo: 'snowplow/factotum',
@@ -19,9 +19,7 @@ export const recipe: Recipe = {
     script: [
       'case {{hw.platform}}+{{hw.arch}} in',
       '  darwin+aarch64) ASSET=factotum_{{version}}_darwin_arm64.zip ;;',
-      '  darwin+x86-64) ASSET=factotum_{{version}}_darwin_x86_64.zip ;;',
       '  linux+x86-64) ASSET=factotum_{{version}}_linux_x86_64.zip ;;',
-      '  linux+aarch64) ASSET=factotum_{{version}}_linux_arm64.zip ;;',
       '  *) echo "unsupported platform {{hw.platform}}/{{hw.arch}}" >&2; exit 1 ;;',
       'esac',
       'curl -Lfo "$ASSET" "https://github.com/snowplow/factotum/releases/download/{{version}}/$ASSET"',
