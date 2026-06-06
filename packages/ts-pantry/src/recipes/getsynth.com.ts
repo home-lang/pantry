20c776d10277b828bfb075bd3adb94b41aeb92f3
Chris <chrisbreuer93@gmail.com>
2026-06-06 14:45:57 -0700

    fix: use official synth binaries

--- packages/ts-pantry/src/recipes/getsynth.com.ts
+++ packages/ts-pantry/src/recipes/getsynth.com.ts
@@ -7,23 +7,32 @@ export const recipe: Recipe = {
   homepage: 'https://www.getsynth.com/',
   github: 'https://github.com/shuttle-hq/synth',
   programs: ['synth'],
+  platforms: ['darwin/x86-64', 'linux'],
   versionSource: {
     type: 'github-releases',
     repo: 'shuttle-hq/synth',
     tagPattern: /^v(.+)$/,
   },
-  distributable: {
-    url: 'https://github.com/shuttle-hq/synth/archive/refs/tags/v{{version}}.tar.gz',
-    stripComponents: 1,
-  },
-  buildDependencies: {
-    'rust-lang.org': '^1.65',
-    'rust-lang.org/cargo': '*',
-  },
+  // Prebuilt download: Synth ships official release tarballs for linux
+  // x86-64/aarch64 and macOS x86-64. There is no macOS arm64 asset.
+  distributable: null,
 
   build: {
     script: [
-      'cargo install --path synth --root {{prefix}}',
+      'case {{hw.platform}}+{{hw.arch}} in',
+      '  darwin+x86-64)  ASSET=synth-macos-latest-x86_64.tar.gz ;;',
+      '  linux+aarch64)  ASSET=synth-ubuntu-latest-arm64.tar.gz ;;',
+      '  linux+x86-64)   ASSET=synth-ubuntu-latest-x86_64.tar.gz ;;',
+      'esac',
+      '',
+      'curl -Lfo synth.tar.gz "https://github.com/shuttle-hq/synth/releases/download/v{{version}}/${ASSET}"',
+      'tar xzf synth.tar.gz',
+      'install -Dm755 synth {{prefix}}/bin/synth',
+    ],
+  },
+  test: {
+    script: [
+      'synth --version',
     ],
   },
 }
